7ebab5ffc1393ac9b23fca6e6b030d72502a8097
Seokho Jeong <kreel9402@gmail.com>
2023-07-11 18:28:13 +0900

    add 프로젝트 생성

---
+++ CoffeeChat_Ver2/CoffeeChat_Ver2/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  CoffeeChat_Ver2
+//
+//  Created by 정유진 on 2023/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
